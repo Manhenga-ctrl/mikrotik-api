f40e3339cddcf06a12c4b4433500e202d93b969f
Manhenga-ctrl <achiponda@linus.co.zw>
2026-02-20 16:51:27 +0200

    commit

--- main.py
+++ main.py
@@ -21,7 +21,25 @@ load_dotenv()
 app = FastAPI(title="MIKROTIK ROUTER MANAMGEMENT API", version="1.0.0")
 
 # CORS – restrict in production!
-app.add_middleware(CORSMiddleware,allow_origins=[ "http://localhost:5173","http://127.0.0.1:5173", ],allow_credentials=True, allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"], allow_headers=["Content-Type", "Authorization"],)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://209.97.148.247",
+        "http://209.97.148.247:8000",
+        "https://yourdomain.com"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
